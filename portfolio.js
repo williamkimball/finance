4677cc9fd1a0aba91e4bda223a8dba94bdf9c1c6
William Kimball <wkkimball043@gmail.com>
2018-06-12 11:09:06 -0500

    finished practice exercise

---
+++ portfolio.js
@@ -0,0 +1,75 @@
+
+const financeAdvisor = Object.create(null, {
+    company: {
+        value: "Heirloom Investments",
+        enumerable: true,
+        writable: true,
+    },
+    specialty: {
+        value: "Making lots of money",
+        enumerable: true,
+        writable: true,
+    },
+    name: {
+        value: "Sarah Dodd",
+        enumerable: true,
+    },
+    portfolio: {
+        value: [
+            {
+                "name": "APPL",
+                "shares": 152,
+                valuation: 30400
+            },
+            {
+                "name": "TSLA",
+                "shares": 198,
+                valuation: 96432
+            },
+            {
+                "name": "GE",
+                "shares": 432,
+                valuation: 10423
+            },
+        ],
+        writable: true
+    },
+    worth: {
+        value:
+            function () {
+                let totalValue = 0;
+                let portfolioArray = financeAdvisor.portfolio;
+                portfolioArray.forEach(element => {
+                    totalValue += element.valuation
+                });
+                return totalValue
+            }
+    },
+    purchase: {
+        value: function (ticker, quantity, price) {
+            let portfolioArray = financeAdvisor.portfolio;
+            portfolioArray.push({
+                "name": `${ticker}`,
+                shares: quantity,
+                valuation: price
+            })
+        }
+    },
+    sell: {
+        value: function (ticker, quantity, price) {
+            let portfolioArray = financeAdvisor.portfolio;
+            let newValue = {};
+
+            portfolioArray.forEach(function (element) {
+                console.log(element)
+                if (ticker === element.name) {
+                    element.shares -= quantity;
+                    element.valuation -= price;
+                } else {
+                    console.log("Please input owned stock")
+                }
+            });
+            return portfolioArray;
+        }
+    }
+});
